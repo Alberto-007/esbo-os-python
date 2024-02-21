f354622d0b6da4e6efdec9879f382ec73432788d
Alberto <139139078+Alberto-007@users.noreply.github.com>
2024-02-21 15:00:25 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,178 @@
+# choice é o nome de uma função do módulo random
+#def nome_da_funçao():
+#em random.choice(lista) lista é o parâmetro da função
+#def- definir uma função
+
+'''media1 = 0.0
+media2 = 0.0
+media3 = 0.0
+media4 = 0.0
+
+def calcular_media_final():
+    media_final = (media1+media2+media3+media4)/4
+    print(f'Média final = {media_final}')
+
+def main():
+    global media1,media2,media3,media4
+# escopo de variável
+    media1 = float(input('Digite a primeira nota: '))
+    media2 = float(input('Digite a segunda média: '))
+    media3 = float(input('Digite a terceira nota: '))
+    media4 = float(input('Digite a quarta nota: '))
+    calcular_media_final()
+
+if __name__ == '__main__':
+    main()'''
+
+#---------------------------------
+
+'''def soma():
+
+
+    if(opcao == 1):
+        print('>> Olá, vamos realizar uma soma <<')
+        num1 = float(input('Digite o primeiro número: '))
+        num2 = float(input('Digite o segundo número: '))
+        print(f'Soma = {num1 + num2}')
+
+def main():
+    print('** OPERAÇÕES MATEMÁTICAS **')
+    print('Opções:')
+    print('1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão')
+    opcao = int(input('Escolha uma opção: '))
+
+    if opcao == 1:
+        soma()
+
+if __name__ == '__main__':
+    main()
+
+def subtracao():
+
+
+    if(opcao == 2):
+        print('>> Olá, vamos realizar uma subtração <<')
+        num1 = float(input('Digite o primeiro número: '))
+        num2 = float(input('Digite o segundo número: '))
+        print(f'Subtração = {num1 - num2}')
+
+def main():
+    print('** OPERAÇÕES MATEMÁTICAS **')
+    print('Opções:')
+    print('1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão')
+    opcao = int(input('Escolha uma opção: '))
+
+    if opcao == 2:
+        subtracao()
+
+if __name__ == '__main__':
+    main()
+
+def multiplicacao():
+
+
+    if(opcao == 3):
+        print('>> Olá, vamos realizar uma multiplicação <<')
+        num1 = float(input('Digite o primeiro número: '))
+        num2 = float(input('Digite o segundo número: '))
+        print(f'Soma = {num1 * num2}')
+
+def main():
+    print('** OPERAÇÕES MATEMÁTICAS **')
+    print('Opções:')
+    print('1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão')
+    opcao = int(input('Escolha uma opção: '))
+
+    if opcao == 3:
+        multiplicacao()
+
+if __name__ == '__main__':
+    main()
+
+def divisao():
+
+
+    if(opcao == 4):
+        print('>> Olá, vamos realizar uma divisão <<')
+        num1 = float(input('Digite o primeiro número: '))
+        num2 = float(input('Digite o segundo número: '))
+        print(f'Soma = {num1 / num2}')
+
+def main():
+    print('** OPERAÇÕES MATEMÁTICAS **')
+    print('Opções:')
+    print('1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão')
+    opcao = int(input('Escolha uma opção: '))
+
+    if opcao == 4:
+        soma()
+
+if __name__ == '__main__':
+    main()'''
+
+
+
+#def soma():
+
+def main():
+     print('** OPERAÇÕES MATEMÁTICAS **')
+     print('Opções:')
+     print('1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão')
+     opcao = int(input('Escolha uma opção: '))
+
+     if(opcao == 1):
+        print('>> Olá, vamos realizar uma soma <<')
+        num1 = float(input('Digite o primeiro número: '))
+        num2 = float(input('Digite o segundo número: '))
+        print(f'Soma = {num1 + num2}')
+
+     if opcao == 1:
+        soma()
+
+'''if __name__ == '__main__':
+    main()
+
+#def subtracao():
+
+
+     if(opcao == 2):
+        print('>> Olá, vamos realizar uma subtração <<')
+        num1 = float(input('Digite o primeiro número: '))
+        num2 = float(input('Digite o segundo número: '))
+        print(f'Subtração = {num1 - num2}')
+
+        if opcao == 2:
+            subtracao()
+
+if __name__ == '__main__':
+    main()
+
+#def multiplicacao():
+
+
+     if(opcao == 3):
+        print('>> Olá, vamos realizar uma multiplicação <<')
+        num1 = float(input('Digite o primeiro número: '))
+        num2 = float(input('Digite o segundo número: '))
+        print(f'Soma = {num1 * num2}')
+
+        if opcao == 3:
+            multiplicacao()
+
+f __name__ == '__main__':
+    main()
+
+#def divisao():
+
+
+         if(opcao == 4):
+            print('>> Olá, vamos realizar uma divisão <<')
+            num1 = float(input('Digite o primeiro número: '))
+            num2 = float(input('Digite o segundo número: '))
+            print(f'Soma = {num1 / num2}')
+
+         if opcao == 4:
+            soma()'''
+
+if __name__ == '__main__': 
+    main()
